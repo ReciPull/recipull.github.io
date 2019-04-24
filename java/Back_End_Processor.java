45d9d36836f537c210df3ece98140c964736e478
Allen Yao <xueyiyao>
2019-04-24 02:49:08 -0700

    Added Back_End_Processor and Database classes

--- java/Back_End_Processor.java
+++ java/Back_End_Processor.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 public class Back_End_Processor{
     public static void main(String[] args){
-        //comments
+        //commentss
         //Class.forName("");
         //Connection con = DriverManager.getConnection("url here", "user here", "password here");
         //from https://www.javatpoint.com/example-to-connect-to-the-mysql-database
