d6d443ae9bc10513937fbf9e9f6daa07c00e701f
Austin Wong <austinwong@umail.ucsb.edu>
2019-04-26 00:18:57 -0700

    Ingredient List Ready

---
+++ recipull/src/components/IngredientSection.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import Ingredients from './Ingredients';
+import uuid from 'uuid'
+import { isAbsolute } from 'path';
+
+class IngredientSection extends Component {
+    state = {
+        ingredientList: []
+      }
+
+    addIngredient = (title) => {
+        const newIngredient = {
+            id: uuid.v4(),
+            title
+        }
+        this.setState({ ingredientList: [...this.state.ingredientList, newIngredient]});
+    }
+
+    delIngredient = (id) => {
+        this.setState({ ingredientList: [...this.state.ingredientList.filter(ingredient => ingredient.id !== id)]})
+    }
+
+    clearAll = () => {
+        this.setState({ingredientList: []})
+    }
+
+    render() {
+        return (
+            <div className='searchSection'>
+                <div className = 'boxtop'>
+                    <SearchBar addIngredient={this.addIngredient}/>
+                </div>
+                <div className= 'ingredList'>
+                    <Ingredients ingredientList = {this.state.ingredientList} delIngredient={this.delIngredient}/>
+                </div>
+                <div className = 'boxBot'>
+                    <p style={tail}>Add Ingredients Here <button className = 'clearBtn' onClick={this.clearAll}> Clear All </button></p>
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+const tail = {
+        backgroundColor: '#f4f4f4',
+        padding: "0px 5px"
+}
+
+export default IngredientSection
