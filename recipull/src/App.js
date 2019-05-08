b619ab679528f6b258a83bcceb144b218664e665
Austin Wong <austinwong@umail.ucsb.edu>
2019-05-08 03:53:02 -0700

    Preliminary Site

--- recipull/src/App.js
+++ recipull/src/App.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import './App.css'
-import Card from './components/Card/Card';
+import IngredientSection from './components/IngredientSection';
+import logo from './components/imgs/recipull_circle.png'
+
+import CardSection from './components/Card/CardSection';
 
 class App extends React.Component {
     render() {
-
-    function addCard(category, imageURL, date, title, text) {
-        return(
-            <Card category={category} image={imageURL} date={date} title={title} text={text}/>
-        )
-    }
-        return(
-            <div className="inLine">
-                <Card category="Lunch" image='https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/2048x1024/landscape-1506456062-delish-spaghetti-meatballs.jpg?resize=1200:*' date="April 23, 2019"
-                    title='Spaghetti & Meatballs' 
-                    text='Basic recipe for Spaghetti & Meatballs. Perfect, quick recipe for a college student.'
-                />
-                <Card category="Breakfast" image='https://www.centercutcook.com/wp-content/uploads/2014/09/fruit-yogurt-granola-parfait-3.jpg' date="April 23, 2019"
-                    title='Yogurt with Granola' 
-                    text='Basic recipe for a balanced breakfast meal. Perfect, quick recipe for a college student to jumpstart their day.'
-                />
-                {addCard("Dinner","https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/2048x1024/landscape-1506456062-delish-spaghetti-meatballs.jpg?resize=1200:*", "April 23, 2019", "Spaghetti","This is a Spaghetti recipe")}
-                {addCard("Lunch","https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/2048x1024/landscape-1506456062-delish-spaghetti-meatballs.jpg?resize=1200:*", "April 23, 2019", "Spaghetti","This is a Spaghetti recipe")}
+        return (
+          <div className="App">
+            <div className="TitleBar">
+                <img src={logo}/>
             </div>
-        )
-    }
+            <div className="MainSectors">
+                <div className="IngredientSector">
+                    
+                    <IngredientSection />
+                </div>
+                <div className="CardSector">
+                    <CardSection />
+                </div>
+            </div>
+          </div>
+        );
+      }
 }
 
 export default App; 
