e91841c9474c1e6e77d7d22ecb63922417e30043
Justin Dong <justindong@Justins-Macbook-Pro.local>
2019-05-08 23:42:32 -0700

    Updates

---
+++ precache-manifest.758d51f75cafadb7ab213b6115095070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1abc6975a65f3d27f58aed6ff87a9f",
+    "url": "/recipull.github.io/index.html"
+  },
+  {
+    "revision": "19f4c4e97815eaad3f88",
+    "url": "/recipull.github.io/static/css/main.8ce50f84.chunk.css"
+  },
+  {
+    "revision": "2e357592d7340ffaca6a",
+    "url": "/recipull.github.io/static/js/2.a3149c4d.chunk.js"
+  },
+  {
+    "revision": "19f4c4e97815eaad3f88",
+    "url": "/recipull.github.io/static/js/main.beb74ef2.chunk.js"
+  },
+  {
+    "revision": "cab5f2e3c8c2c9ca30d5",
+    "url": "/recipull.github.io/static/js/runtime~main.b6f9fe94.js"
+  },
+  {
+    "revision": "cf5917f1b1be4497ac84d94b6e0ef73f",
+    "url": "/recipull.github.io/static/media/recipull_circle.cf5917f1.png"
+  }
+]);
