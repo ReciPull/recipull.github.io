9b42bbf12d40299ca255051473ffa2ce4f7b0558
Justin Dong <justindong@169-231-140-22.wireless.ucsb.edu>
2019-05-22 16:30:45 -0700

    completed functionality for recipe output

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -31,7 +31,7 @@ class CardBody extends React.Component {
         return (
         <div className="fullBody">
             <div className="card-body">
-                <p className="date">{this.props.numIngredients} Ingredients</p>
+                <p className="date">{this.props.numIngredients}</p>
                 
                 <h2>{this.props.title}</h2>
                 
